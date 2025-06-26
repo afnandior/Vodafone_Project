cbae497cb4a7756e257b71691de90c3e4e4c154f
afnandior <611afnanmohamed@gmail.com>
2025-06-26 03:11:35 +0300

    Update clustering.py

--- models/clustering.py
+++ models/clustering.py
@@ -1,1 +1,12 @@
+from sklearn.cluster import KMeans, DBSCAN
+
+def kmeans_clustering(X, n_clusters):
+    model = KMeans(n_clusters=n_clusters, n_init=10)
+    y_pred = model.fit_predict(X)
+    return model, y_pred
+
+def dbscan_clustering(X, eps, min_samples):
+    model = DBSCAN(eps=eps, min_samples=min_samples)
+    y_pred = model.fit_predict(X)
+    return model, y_pred
 
