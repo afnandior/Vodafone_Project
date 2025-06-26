1ddd5f7b6efc670c8f7ae81011f67607dabc4346
afnandior <611afnanmohamed@gmail.com>
2025-06-26 03:06:58 +0300

    Update classification.py

--- models/classification.py
+++ models/classification.py
@@ -1,1 +1,13 @@
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+
+def logistic_regression(X, y):
+    model = LogisticRegression().fit(X, y)
+    y_pred = model.predict(X)
+    return model, y_pred
+
+def decision_tree_classifier(X, y):
+    model = DecisionTreeClassifier().fit(X, y)
+    y_pred = model.predict(X)
+    return model, y_pred
 
