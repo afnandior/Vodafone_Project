627c0ee1ef5dde4e5d9dbdaf38a8b640e5cd03d1
afnandior <611afnanmohamed@gmail.com>
2025-06-28 05:20:58 +0300

    Update classification.py

--- models/classification.py
+++ models/classification.py
@@ -4,12 +4,12 @@ from sklearn.tree import DecisionTreeClassifier
 def logistic_regression(X, y):
     model = LogisticRegression().fit(X, y)
     y_pred = model.predict(X)
-    return model, y_pred
+    return model, y_pred, {}
 
 def decision_tree_classifier(X, y):
     model = DecisionTreeClassifier().fit(X, y)
     y_pred = model.predict(X)
-    return model, y_pred
+    return model, y_pred, {}
 
 # 🔥 Main Controller Function
 def handle_classification_models(model_type, X, y):
@@ -17,13 +17,8 @@ def handle_classification_models(model_type, X, y):
     Handles classification model selection and execution.
     """
     if model_type == "Logistic Regression":
-        model, y_pred = logistic_regression(X, y)
-        return model, y_pred, {}
-    
+        return logistic_regression(X, y)
     elif model_type == "Decision Tree Classification":
-        model, y_pred = decision_tree_classifier(X, y)
-        return model, y_pred, {}
-    
+        return decision_tree_classifier(X, y)
     else:
         return None, None, {}
-
