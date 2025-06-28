9674c1c1e1c29589016cef4fbde8319c0e41167c
afnandior <611afnanmohamed@gmail.com>
2025-06-28 05:05:47 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,6 @@
 import sys
 import os
 sys.path.append(os.path.abspath(os.path.dirname(__file__)))
-
 import streamlit as st
 from data_loader import load_data
 from models.regression import handle_regression_models
