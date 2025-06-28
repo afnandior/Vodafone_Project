1db874a9a88ca67fa9a003fa80243457317edcdd
afnandior <611afnanmohamed@gmail.com>
2025-06-28 04:02:16 +0300

    Update pca.py

--- models/pca.py
+++ models/pca.py
@@ -1,6 +1,15 @@
 from sklearn.decomposition import PCA
 
-def run_pca(X, n_components):
-    model = PCA(n_components=n_components)
-    X_reduced = model.fit_transform(X)
-    return model, X_reduced
+def pca_reduction(X, n_components):
+    pca = PCA(n_components=n_components)
+    X_reduced = pca.fit_transform(X)
+    return X_reduced, pca
+
+# 🔥 Main Controller Function
+def handle_pca(X, n_components=None):
+    """
+    Handles PCA dimensionality reduction.
+    """
+    n_components = n_components or 2
+    X_reduced, pca = pca_reduction(X, n_components)
+    return X_reduced
